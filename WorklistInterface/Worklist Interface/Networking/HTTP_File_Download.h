2270a45a42f953795a59e369244194d4d0107094
Karel <karelgerbrands@gmail.com>
2019-02-06 15:27:47 +0100

    #117
    - Adds documentation to the HTTP downloading

--- WorklistInterface/Worklist Interface/Networking/HTTP_File_Download.h
+++ WorklistInterface/Worklist Interface/Networking/HTTP_File_Download.h
@@ -18,11 +18,11 @@ namespace ASAP::Worklist::Networking
 	enum DOWNLOAD_STATUS { DOWNLOAD_FAILURE, FILE_CREATION_FAILURE, NO_ATTACHMENT, SUCCESS };
 
 	/// <summary>
-	/// 
+	/// Downloads a file that may be tied to a HTTP response.
 	/// </summary>
-	/// <param name="response"></param>
-	/// <param name="directory"></param>
-	/// <return></return>
+	/// <param name="response">The response holding the file.</param>
+	/// <param name="output_directory">The directory to write the image to.</param>
+	/// <return>A struct containing the absolute path to the downloaded file (empty on failure) and the state of the download.</return>
 	FileDownloadResults HTTP_File_Download(const web::http::http_response& response, const boost::filesystem::path& output_directory);
 }
 #endif // __ASAP_NETWORKING_HTTPFILEDOWNLOAD__
