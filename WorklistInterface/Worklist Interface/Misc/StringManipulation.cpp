8fbfb8997da335130a8286ddb225e8ced65da957
Karel <karelgerbrands@gmail.com>
2019-03-04 09:55:55 +0100

    - Improves the split method
    #124

--- WorklistInterface/Worklist Interface/Misc/StringManipulation.cpp
+++ WorklistInterface/Worklist Interface/Misc/StringManipulation.cpp
@@ -11,7 +11,7 @@ namespace ASAP::Misc
 		bool within_encapsulation = false;
 		for (size_t current_char = 0; current_char < string.size(); ++current_char)
 		{
-			if (string[current_char] == '"')
+			if (string[current_char] == encapsulation)
 			{
 				within_encapsulation = !within_encapsulation;
 				if (within_encapsulation)
@@ -23,7 +23,7 @@ namespace ASAP::Misc
 					value_end = current_char;
 				}
 			}
-			else if (string[current_char] == ',' && value_end != 0)
+			else if (string[current_char] == delimiter && value_end == 0)
 			{
 				value_end = current_char;
 			}
@@ -32,9 +32,16 @@ namespace ASAP::Misc
 			{
 				split_string.push_back(string.substr(value_start, value_end - value_start));
 				value_start = current_char + 1;
-				value_end = 0;
+				value_end	= 0;
 			}
 		}
+
+		if (value_start < string.size() && (string[string.size() - 1] != delimiter || string[string.size() - 1] != encapsulation))
+		{
+			value_end = string.size();
+			split_string.push_back(string.substr(value_start, value_end - value_start));
+		}
+
 		return split_string;
 	}
 }
