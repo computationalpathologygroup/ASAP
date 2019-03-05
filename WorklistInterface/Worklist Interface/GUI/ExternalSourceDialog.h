03914d2a451cd123046be923db1f8564bee5dafd
Karel <karelgerbrands@gmail.com>
2019-03-05 10:04:28 +0100

    - Adds the external source dialog
    #124

--- WorklistInterface/Worklist Interface/GUI/ExternalSourceDialog.h
+++ WorklistInterface/Worklist Interface/GUI/ExternalSourceDialog.h
@@ -2,6 +2,12 @@
 #define __ASAP_GUI_EXTERNALSOURCEDIALOG__
 
 #include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include <QDialogButtonBox>
+#include <QLineEdit>
+#include <QGridLayout>
+#include <QCheckBox>
 
 namespace ASAP::GUI
 {
@@ -9,10 +15,36 @@ namespace ASAP::GUI
 	{
 		Q_OBJECT
 		public:
-			ExternalSourceDialog(void);
+			struct SourceDialogResults
+			{
+				QString location;
+				QString token;
+				bool	ignore_certificate;
+			};
 
-		signals:
-			void AcquiredLoginDetails(const QString& location, const QString& username, const QString& password);
+			explicit ExternalSourceDialog(QWidget* parent = nullptr);
+
+			SourceDialogResults GetLoginDetails(void);
+			bool HasValidCredentials(void);
+
+
+		private:
+			QLabel*				m_label_location_;
+			QLabel*				m_label_token_;
+
+			QLineEdit*			m_input_location_;
+			QLineEdit*			m_input_token_;
+			QCheckBox*			m_input_ignore_certificate_;
+
+			QDialogButtonBox*	m_buttons_;
+			QGridLayout*		m_grid_layout_;
+
+			bool				m_valid_;
+
+			void SetupGUI_(void);
+
+		private slots:
+			void SaveCredentials_(void);
 	};
 }
 #endif // __ASAP_GUI_EXTERNALSOURCEDIALOG__
