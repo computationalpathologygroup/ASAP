07776e379d87652c92e498e2c4a040de115d5152
Karel <karelgerbrands@gmail.com>
2019-03-13 14:45:06 +0100

    - Adds basic document class
    #130

---
+++ ASAP/documents/Document.cpp
@@ -0,0 +1,66 @@
+#include "Document.h"
+
+#include <stdexcept>
+
+#include <multiresolutionimageinterface/MultiResolutionImageReader.h>
+
+namespace ASAP::Documents
+{
+	Document::Document(const std::string& filepath, const std::string& factory) : m_filepath_(filepath), m_image_(nullptr)
+	{
+		// Ensures parameters have been set.
+		if (filepath.empty())
+		{
+			throw std::invalid_argument("Filepath is empty.");
+		}
+		if (factory.empty())
+		{
+			throw std::invalid_argument("No factory selected");
+		}
+
+		// Attempts to open the file.
+		MultiResolutionImageReader reader;
+		m_image_.reset(reader.open(filepath, factory));
+
+		// Checks if the image is valid.
+		if (!m_image_)
+		{
+			throw std::runtime_error("Invalid file type");
+		}
+		if (!m_image_->valid())
+		{
+			throw std::runtime_error("Unsupported file type version");
+		}
+
+		// TODO: Ask why this is being done.
+		std::vector<unsigned long long> dimensions = m_image_->getLevelDimensions(m_image_->getNumberOfLevels() - 1);
+	}
+
+	const std::string& Document::GetFilepath(void)
+	{
+		return m_filepath_;
+	}
+
+	const MultiResolutionImage* Document::GetImage(void)
+	{
+		return m_image_.get();
+	}
+
+	PluginInformation* Document::GetPluginInformation(const std::string& plugin)
+	{
+		return m_plugin_information_[plugin].get();
+	}
+
+	bool Document::HasPluginInformation(const std::string& plugin)
+	{
+		return m_plugin_information_.find(plugin) != m_plugin_information_.end();
+	}
+
+	void Document::SetPluginInformation(const std::string& plugin, PluginInformation* information, const bool allow_override)
+	{
+		if (allow_override || m_plugin_information_.find(plugin) == m_plugin_information_.end())
+		{
+			m_plugin_information_.insert({ plugin, std::unique_ptr<PluginInformation>(information) });
+		}
+	}
+}
---
+++ ASAP/documents/Document.h
@@ -0,0 +1,55 @@
+#ifndef __ASAP_DOCUMENTS_DOCUMENT__
+#define __ASAP_DOCUMENTS_DOCUMENT__
+
+#include "asaplib_export.h"
+
+#include <qrect.h>
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <multiresolutionimageinterface/MultiResolutionImage.h>
+
+class PluginInformation
+{
+	
+};
+
+
+
+typedef std::unordered_map<uint32_t, std::unordered_map<int32_t, std::unordered_map<int32_t, uchar>>> CoverageMap;
+
+struct TileInformation
+{
+	const uint64_t						tile_size;
+	uint64_t							last_level;
+	uint64_t							last_render_level;
+	QRect								last_FOV;
+	std::vector<float>					downsamples;
+	std::vector<std::vector<uint64_t>>	dimensions;
+	CoverageMap							coverage;
+};
+
+namespace ASAP::Documents
+{
+	class Document
+	{
+		public:
+			Document(const std::string& filepath, const std::string& factory = "default");
+
+			const std::string& GetFilepath(void);
+			const MultiResolutionImage* GetImage(void);
+
+			PluginInformation* GetPluginInformation(const std::string& plugin);
+			bool HasPluginInformation(const std::string& plugin);
+			void SetPluginInformation(const std::string& plugin, PluginInformation* information, const bool allow_override = false);
+
+		private:
+			std::string								m_filepath_;
+			std::unique_ptr<MultiResolutionImage>	m_image_;
+			std::unordered_map<std::string, std::unique_ptr<PluginInformation>> m_plugin_information_;
+	};
+}
+#endif // __ASAP_DOCUMENTS_DOCUMENT__
