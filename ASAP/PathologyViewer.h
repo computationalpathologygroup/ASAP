a18c9f8a8b6cd80f67c44deacefb078f2090a143
Karel <karelgerbrands@gmail.com>
2019-06-04 16:11:06 +0200

    - Cleans up the PathologyViewer header file - Refactors header to accept DocumentInstance
    #152

--- ASAP/PathologyViewer.h
+++ ASAP/PathologyViewer.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <memory>
 
-#include "documents/Document.h"
+#include "documents/DocumentInstance.h"
 
 class MultiResolutionImage;
 class RenderThread;
@@ -21,122 +21,114 @@ class ASAPLIB_EXPORT PathologyViewer : public QGraphicsView
 {
     Q_OBJECT
     
-public:
+	public:
+		QPointF _zoomToScenePos;
+		QPointF _zoomToViewPos;
 
-    QPointF _zoomToScenePos;
-    QPointF _zoomToViewPos;
+		PathologyViewer(QWidget *parent = 0);
+		~PathologyViewer(void);
 
-    PathologyViewer(QWidget *parent = 0);
-    ~PathologyViewer();
+		void initialize(ASAP::DocumentInstance& instance);
+		void close(void);
 
-    void initialize(ASAP::Document& document);
-    void close();
+		float getZoomSensitivity() const;
+		float getPanSensitivity() const;
+		void  setZoomSensitivity(float zoomSensitivity);
+		void  setPanSensitivity(float panSensitivity);
 
-    float getZoomSensitivity() const;
-    void  setZoomSensitivity(float zoomSensitivity);
-    void  setPanSensitivity(float panSensitivity);
-    float getPanSensitivity() const;
+		float getForegroundOpacity() const;
+		void setForegroundOpacity(const float& opacity);
+		void setForegroundWindowAndLevel(const float& window, const float& level);
+		void setForegroundLUT(const std::string& LUTname);
+		void setForegroundChannel(unsigned int channel);
 
-    float getForegroundOpacity() const;
-    void  setForegroundOpacity(const float& opacity);
-
-    void setForegroundWindowAndLevel(const float& window, const float& level);
-    void setForegroundLUT(const std::string& LUTname);
-    void setForegroundChannel(unsigned int channel);
-
-    void togglePan(bool pan, const QPoint& startPos = QPoint());
-    void pan(const QPoint& panTo);
-    bool isPanning();
-    void zoom(float numSteps);
+		void togglePan(bool pan, const QPoint& startPos = QPoint());
+		void pan(const QPoint& panTo);
+		bool isPanning();
+		void zoom(float numSteps);
    
-    bool hasTool(const std::string& toolName) const;
-    void addTool(std::shared_ptr<ToolPluginInterface> tool);
-    void setActiveTool(const std::string& toolName);
-    std::shared_ptr<ToolPluginInterface> getActiveTool();
-    float getSceneScale() { return _sceneScale; }
+		bool hasTool(const std::string& toolName) const;
+		void addTool(std::shared_ptr<ToolPluginInterface> tool);
+		void setActiveTool(const std::string& toolName);
+		std::shared_ptr<ToolPluginInterface> getActiveTool(void);
+		float getSceneScale(void) const;
 
-    unsigned long long getCacheSize();
-    void setCacheSize(unsigned long long& maxCacheSize);
-    void setAutoUpdate(bool autoUpdate);
+		unsigned long long getCacheSize(void) const;
+		void setCacheSize(unsigned long long& maxCacheSize);
+		void setAutoUpdate(bool autoUpdate);
 
-    void updateCurrentFieldOfView();
+		void updateCurrentFieldOfView();
 
-signals :
-    void fieldOfViewChanged(const QRectF& FOV, const unsigned int level);
-    void updateBBox(const QRectF& FOV);
-    void backgroundChannelChanged(int channelNr);
+	signals :
+		void fieldOfViewChanged(const QRectF& FOV, const unsigned int level);
+		void updateBBox(const QRectF& FOV);
+		void backgroundChannelChanged(int channelNr);
 
-public slots :
-    void moveTo(const QPointF& pos);
-    void changeActiveTool();
-    void onFieldOfViewChanged(const QRectF& FOV, const unsigned int level);
-    void onForegroundImageChanged(std::weak_ptr<MultiResolutionImage> for_img, float scale);
+	public slots :
+		void moveTo(const QPointF& pos);
+		void changeActiveTool();
+		void onFieldOfViewChanged(const QRectF& FOV, const unsigned int level);
+		void onForegroundImageChanged(std::weak_ptr<MultiResolutionImage> for_img, float scale);
 
-private :
-    // Functions for panning
-    virtual void mouseMoveEvent(QMouseEvent *event);
-    virtual void mousePressEvent(QMouseEvent *event);
-    virtual void mouseReleaseEvent(QMouseEvent *event);
-    virtual void mouseDoubleClickEvent(QMouseEvent *event);
-    virtual void keyPressEvent(QKeyEvent *event);
-    virtual void resizeEvent(QResizeEvent *event);
+	private slots:
+		void showContextMenu(const QPoint& pos);
+		void scalingTime(qreal x);
+		void zoomFinished();
 
-    // Functions for zooming and resizing
-    void wheelEvent(QWheelEvent *event);    
+	private :
+		//float _sceneScale;
 
-    // Functions for overviewmap
-    void initializeGUIComponents(unsigned int level);
-    void initializeImage(QGraphicsScene *scn, unsigned int tileSize, unsigned int lastLevel);
-    float _sceneScale;
+		// Interface to the multi-resolution image. Please note that PathologyViewer
+		// should never modify _img (it is modified in the RenderThread due to calls
+		// to readRegion), otherwise race conditions could occur.
+		ASAP::DocumentInstance* m_active_instance_;
+		std::weak_ptr<MultiResolutionImage> _for_img;
 
-    // Interface to the multi-resolution image. Please note that PathologyViewer
-    // should never modify _img (it is modified in the RenderThread due to calls
-    // to readRegion), otherwise race conditions could occur.
-	ASAP::Document* m_active_document_;
-    std::weak_ptr<MultiResolutionImage> _for_img;
+		// Internals
+		unsigned long long			_cacheSize;
+		WSITileGraphicsItemCache*	_cache;
+		TileManager*				_manager;
+		PrefetchThread*				_prefetchthread;
 
-    // Minimap
-    MiniMap* _map;
+		// UI
+		MiniMap*	_map;
+		ScaleBar*	_scaleBar;
+		QSettings*	_settings;
 
-    // ScaleBar
-    ScaleBar* _scaleBar;
+		// Tools
+		std::shared_ptr<ToolPluginInterface> _activeTool;
+		std::map<std::string, std::shared_ptr<ToolPluginInterface> > _tools;
 
-    // QSettings
-    QSettings* _settings;
+		// Members to track panning and zooming
+		float _zoomSensitivity;
+		float _numScheduledScalings;
+		float _panSensitivity;
+		bool _pan;
+		QPoint _prevPan;
 
-    // Tools
-    std::shared_ptr<ToolPluginInterface> _activeTool;
+		// Members related to rendering
+		RenderThread* _renderthread;
+		int _backgroundChannel;
+		int _foregroundChannel;
+		float _opacity;
+		float _window;
+		float _level;
+		std::string _LUTname;
+		float _foregroundImageScale;
 
-    // Members to track panning and zooming
-    float _zoomSensitivity;
-    float _numScheduledScalings;
-    float _panSensitivity;
-    bool _pan;
-    QPoint _prevPan;
+		// Functions for panning
+		virtual void mouseMoveEvent(QMouseEvent *event);
+		virtual void mousePressEvent(QMouseEvent *event);
+		virtual void mouseReleaseEvent(QMouseEvent *event);
+		virtual void mouseDoubleClickEvent(QMouseEvent *event);
+		virtual void keyPressEvent(QKeyEvent *event);
+		virtual void resizeEvent(QResizeEvent *event);
 
-    // Members related to rendering
-    RenderThread* _renderthread;
-    int _backgroundChannel;
-    int _foregroundChannel;
-    float _opacity;
-    float _window;
-    float _level;
-    std::string _LUTname;
-    float _foregroundImageScale;
+		// Functions for zooming and resizing
+		void wheelEvent(QWheelEvent *event);    
 
-    PrefetchThread* _prefetchthread;
-
-    TileManager* _manager;
-    unsigned long long _cacheSize;
-    WSITileGraphicsItemCache* _cache;
-
-    std::map<std::string, std::shared_ptr<ToolPluginInterface> > _tools;
-
-private slots :
-    void showContextMenu(const QPoint& pos);
-    void scalingTime(qreal x);
-    void zoomFinished();
+		// Functions for overviewmap
+		void initializeGUIComponents(unsigned int level);
+		void initializeImage(QGraphicsScene *scn, unsigned int tileSize, unsigned int lastLevel);
 };
-  
-
-#endif
+#endif // PATHOLOGYVIEWER_H
