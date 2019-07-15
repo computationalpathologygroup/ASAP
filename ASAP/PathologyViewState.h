8deb6796d9a82dc52cc9718480b52e9120d6ff89
Karel <karelgerbrands@gmail.com>
2019-07-15 13:29:14 +0200

    - Adds foreground image information to the viewstate object
    #157

--- ASAP/PathologyViewState.h
+++ ASAP/PathologyViewState.h
@@ -2,13 +2,31 @@
 #define PATHOLOGY_VIEW_STATE_H
 #include "asaplib_export.h"
 
+#include <memory>
+
 #include <qpoint.h>
+
+#include "multiresolutionimageinterface/MultiResolutionImage.h"
 
 struct ASAPLIB_EXPORT PathologyViewState
 {
+	std::weak_ptr<MultiResolutionImage> foreground_image;
+
+	uint32_t	background_channel;
+	uint32_t	foreground_channel;
+	float		foreground_opacity;
+	float		foreground_window;
+	float		foreground_level;
+	std::string foreground_lut_name;
+	float		foreground_scale;
+
 	QPoint	pan_position;
 	QPointF zoom_view_center;
 	QPointF zoom_scene_center;
 	qreal	zoom_factor;
+
+	PathologyViewState(void) : zoom_factor(1.0f)
+	{
+	}
 };
 #endif // PATHOLOGY_VIEW_STATE_H
