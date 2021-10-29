8f23975e2fc0c3604f98441bdeda06534ba875d9
Geert Litjens <gjslitjens@gmail.com>
2021-10-29 11:30:28 +0200

    Small fixes for Ubuntu 18.04 build

--- ASAP/worklist_interface/GUI/WorklistWindow.h
+++ ASAP/worklist_interface/GUI/WorklistWindow.h
@@ -2,7 +2,6 @@
 #define __ASAP_GUI_WORKLISTDATAACQUISITIONINTERFACE__
 
 #include <deque>
-#include <memory>
 #include <mutex>
 #include <unordered_map>
 #include <QDropEvent>
@@ -48,7 +47,7 @@ namespace ASAP
 		private:
 			SourceProxy									m_source;
 			std::unique_ptr<Ui::WorklistWindowLayout>	m_ui;
-			std::unique_ptr<QFutureWatcher<void> >      m_thumbnail_loader;
+			std::unique_ptr<QFutureWatcher<void>>       m_thumbnail_loader;
 			std::mutex									m_image_switch_access;
 			std::mutex									m_status_bar_access;
 			TemporaryDirectoryTracker					m_storage_directory;
