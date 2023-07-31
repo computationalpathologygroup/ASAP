c976abbf9da4d9af5f5ae25675240b7bc3a54246
Geert Litjens <gjslitjens@gmail.com>
2023-07-31 16:43:32 +0200

    Fixed build for older Qt6 versions

--- buildtools/build_ASAP.sh
+++ buildtools/build_ASAP.sh
@@ -10,35 +10,32 @@ ubuntu_version_no_dots=$(echo ${ubuntu_version} | tr -d ".")
 
 echo "Building ASAP with Python ${python_ver}; building GUI = ${build_gui}; on Ubuntu ${ubuntu_version}"
 git clone https://github.com/computationalpathologygroup/ASAP src
+cd src
+git checkout upgrading_dependencies
+cd ..
 mkdir build
 cd build
 
 if [ "${build_gui}" = "true" ] ; then \
-        cmake ../src -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
-                    -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=TRUE -DCMAKE_INSTALL_PREFIX=/root/install \
-                    -DBUILD_ASAP=TRUE -DBUILD_EXECUTABLES=TRUE -DBUILD_IMAGEPROCESSING=TRUE -DBUILD_MULTIRESOLUTIONIMAGEINTERFACE_VSI_SUPPORT=TRUE -DCMAKE_BUILD_TYPE=Release \
-                    -DDCMTKJPEG_INCLUDE_DIR=/root -DDCMTKJPEG_LIBRARY=/usr/lib/libijg8.so -DUNITTEST_INCLUDE_DIR=/usr/include/UnitTest++ \
-                    -DPACKAGE_ON_INSTALL=TRUE -DBUILD_WORKLIST_INTERFACE=TRUE \
-                    -DSWIG_EXECUTABLE=/root/swig/install/bin/swig \
-                    -DPYTHON_DEBUG_LIBRARY=/root/miniconda3/envs/build_python3.9/lib/libpython3.so \
-                    -DPYTHON_LIBRARY_DEBUG=/root/miniconda3/envs/build_python3.9/lib/libpython3.so \
-                    -DPYTHON_LIBRARY=/root/miniconda3/envs/build_python3.9/lib/libpython3.so \
-                    -DPYTHON_INCLUDE_DIR=/root/miniconda3/envs/build_python3.9/include/python3.9 \
-                    -DPYTHON_EXECUTABLE=/root/miniconda3/envs/build_python3.9/bin/python \
-                    -DPYTHON_NUMPY_INCLUDE_DIR=/root/miniconda3/envs/build_python3.9/lib/python3.9/site-packages/numpy/core/include \
+        cmake ../src -DBUILD_ASAP=TRUE \
+                     -DBUILD_EXECUTABLES=TRUE \
+                     -DBUILD_IMAGEPROCESSING=TRUE \
+                     -DBUILD_WORKLIST_INTERFACE=TRUE \
+                     -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=TRUE \
+                     -DCMAKE_BUILD_TYPE=Release \
+                     -DPACKAGE_ON_INSTALL=TRUE \
+                     -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
+                     -DCMAKE_INSTALL_PREFIX=/root/install \
+                     -DSWIG_EXECUTABLE=/root/swig/install/bin/swig \
     ; else \
         echo "Skipping GUI..."
         cmake ../src -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
-                 -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=TRUE -DCMAKE_INSTALL_PREFIX=/root/install \
-                 -DBUILD_MULTIRESOLUTIONIMAGEINTERFACE_VSI_SUPPORT=TRUE -DCMAKE_BUILD_TYPE=Release \
-                 -DDCMTKJPEG_INCLUDE_DIR=/root -DDCMTKJPEG_LIBRARY=/usr/lib/libijg8.so -DPACKAGE_ON_INSTALL=TRUE \
-                    -DSWIG_EXECUTABLE=/root/swig/install/bin/swig \
-                    -DPYTHON_DEBUG_LIBRARY=/root/miniconda3/envs/build_python3.9/lib/libpython3.so \
-                    -DPYTHON_LIBRARY_DEBUG=/root/miniconda3/envs/build_python3.9/lib/libpython3.so \
-                    -DPYTHON_LIBRARY=/root/miniconda3/envs/build_python3.9/lib/libpython3.so \
-                    -DPYTHON_INCLUDE_DIR=/root/miniconda3/envs/build_python3.9/include/python3.9 \
-                    -DPYTHON_EXECUTABLE=/root/miniconda3/envs/build_python3.9/bin/python \
-                    -DPYTHON_NUMPY_INCLUDE_DIR=/root/miniconda3/envs/build_python3.9/lib/python3.9/site-packages/numpy/core/include \
+                     -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=TRUE \
+                     -DCMAKE_INSTALL_PREFIX=/root/install \
+                     -DBUILD_MULTIRESOLUTIONIMAGEINTERFACE_VSI_SUPPORT=TRUE \
+                     -DCMAKE_BUILD_TYPE=Release \
+                     -DPACKAGE_ON_INSTALL=TRUE \
+                     -DSWIG_EXECUTABLE=/root/swig/install/bin/swig \
     ; fi
 export LD_LIBRARY_PATH=/root/miniconda3/envs/build_python3.9/lib
 make package
